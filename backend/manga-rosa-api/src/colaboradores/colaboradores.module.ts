cd9826f96c8e9d12867c6cd7002ffb66cb920801
ojuliomiguel <juliomiguelsouzacosta@gmail.com>
2021-06-25 19:42:07 -0300

    refact: adicionando entidade conhecimento

--- backend/manga-rosa-api/src/colaboradores/colaboradores.module.ts
+++ backend/manga-rosa-api/src/colaboradores/colaboradores.module.ts
@@ -3,9 +3,13 @@ import { ColaboradoresService } from './colaboradores.service';
 import { ColaboradoresController } from './colaboradores.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Colaborador } from './entities/colaborador.entity';
+import { Conhecimento } from 'src/conhecimentos/conhecimento.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Colaborador])],
+  imports: [
+    TypeOrmModule.forFeature([Colaborador, Conhecimento]),
+    
+  ],
   controllers: [ColaboradoresController],
   providers: [ColaboradoresService]
 })
