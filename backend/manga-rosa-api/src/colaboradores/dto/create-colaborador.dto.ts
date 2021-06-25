4a5a4d60fc032a1af6dfe9c2286f99c8f8926e8e
ojuliomiguel <juliomiguelsouzacosta@gmail.com>
2021-06-25 10:00:16 -0300

    feat: adiciona campos com validação

--- backend/manga-rosa-api/src/colaboradores/dto/create-colaborador.dto.ts
+++ backend/manga-rosa-api/src/colaboradores/dto/create-colaborador.dto.ts
@@ -1,1 +1,19 @@
-export class CreateColaboradorDto {}
+import {IsEmail, IsNotEmpty, IsOptional, Matches} from 'class-validator'
+
+export class CreateColaboradorDto {
+    @IsNotEmpty({message: 'O nome precisa ser fornecido'})
+    nome: string
+    
+    @IsEmail({},{message: 'O email fornecido é inválido'})
+    email: string;
+    
+    @Matches(/^\([1-9]{2}\) (?:[2-8]|9[1-9])[0-9]{3}\-[0-9]{4}$/, {message: 'O cpf informado está em formato inválido'})
+    cpf: string;
+
+    @IsOptional()
+    @Matches(/^\d{3}\.\d{3}\.\d{3}\-\d{2}$/, {message: 'Telefone inserido é inválido'})
+    celular: string;
+
+    @IsOptional()
+    validado: boolean;
+}
