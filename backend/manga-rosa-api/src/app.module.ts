b052afc4c85f874baa2a16237f7adaac396d697d
ojuliomiguel <juliomiguelsouzacosta@gmail.com>
2021-06-27 08:58:18 -0300

    feat: registra modulo de conhecimentos

--- backend/manga-rosa-api/src/app.module.ts
+++ backend/manga-rosa-api/src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ColaboradoresModule } from './colaboradores/colaboradores.module';
+import { ConhecimentoModule } from './conhecimentos/conhecimentos.module';
 
 @Module({
   imports: [
     ColaboradoresModule,
+    ConhecimentoModule,
     TypeOrmModule.forRoot({autoLoadEntities: true,})
   ]
 })
